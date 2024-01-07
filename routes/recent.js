29b4734d74762eb74ca37b25e6ecb80aa024fac1
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-01-07 15:37:12 +0530

    delete

--- routes/recent.js
+++ routes/recent.js
@@ -5,25 +5,40 @@ const Recent = require('../modals/recentModel');
 const protect = require('../middleware/authmiddleware');
 const Crop = require('../modals/recentCropModel');
 
+
 // For storing searched crop names
 router.post('/store_crop_name', protect, expressAsyncHandler(async (req, res, next) => {
     try {
-        const searchExist = await Crop.findOne({Crop:req.body.cropname})
-        if(!searchExist){
-            const search = new Recent({
-                user : req.id,
-                crop : req.body.cropname,
+        const searchExist = await Crop.findOne({ Crop: req.body.cropname })
+        if (!searchExist) {
+
+            const crops = await Crop.find({ user: req.id }).sort({ createdAt: 1 });
+            if (crops.length >= 5) {
+                // Remove the oldest crop
+                await Crop.findByIdAndDelete(crops[0]._id);
+            }
+            const search = new Crop({
+                user: req.id,
+                crop: req.body.cropname,
+            })
+
+            await search.save();
+            res.send({ data: search });
+        }
+        else {
+            await Crop.findOneAndDelete({crop: req.body.cropname })
+            const search = new Crop({
+                user: req.id,
+                crop: req.body.cropname,
             })
             await search.save();
-            res.send({data:search});
-        }
-        else{
-            throw new Error("Crop name exist")
+            res.send({ data: search });
         }
     } catch (error) {
         throw new Error("Crop already exists");
     }
 }))
+
 
 // Form submit to store info of soil 
 router.post('/submit', protect, expressAsyncHandler(async (req, res, next) => {
@@ -49,7 +64,7 @@ router.post('/submit', protect, expressAsyncHandler(async (req, res, next) => {
 
 router.get('/recent_crop', protect, expressAsyncHandler(async (req, res, next) => {
     try {
-        const recentCrop = await Recent.find({ user: req.id }).select('crop').exec();
+        const recentCrop = await Crop.find({ user: req.id });
         res.status(200).send({ recentCrop });
     } catch (error) {
         res.status(500).send({ error: error.message });
@@ -58,7 +73,7 @@ router.get('/recent_crop', protect, expressAsyncHandler(async (req, res, next) =
 
 router.get('/recent_soil', protect, expressAsyncHandler(async (req, res, next) => {
     try {
-        const recentSearch = await Recent.find({ user: req.id }).select('-crop').exec();
+        const recentSearch = await Recent.find({ user: req.id });
         res.status(200).send({ recentSearch });
     } catch (error) {
         res.status(500).send({ error: error.message });
